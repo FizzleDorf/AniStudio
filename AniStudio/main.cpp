9a69df1d874200de9466cd3ea3a829f6c1ddde10
FizzleDorf <1fizzledorf@gmail.com>
2024-07-26 23:08:32 -0400

    Conan package manager added

--- AniStudio/main.cpp
+++ AniStudio/main.cpp
@@ -1,20 +1,62 @@
-#include "pch.h"
-#include "Core/Ani.hpp"
-#include "ECS/ECS.hpp"
-#include "imgui.h"
+#include <glew.h>
+#include <glfw3.h>
+#include <imgui>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
-int main(int argc, char** argv) {
-	
-	ANI::Core.Init();
-	ANI::Timer.Init();
-	ANI::Event.Init();
-	
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        return -1;
+    }
 
-	while (ANI::Core.Run()) {
-		ANI::Timer.Tick();
-		ANI::Event.Poll();
-		ANI::Core.Update();
-	}
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "AniStudio", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 
-	return EXIT_SUCCESS;
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        return -1;
+    }
+
+    // Setup ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Your ImGui code here
+        ImGui::Begin("Hello, AniStudio!");
+        ImGui::Text("This is a simple text.");
+        ImGui::End();
+
+        // Rendering
+        ImGui::Render();
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup ImGui
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
