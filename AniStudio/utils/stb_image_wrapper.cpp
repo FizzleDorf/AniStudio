ad7684f1f31d60557fb35fa87ebf67e3a9b26d23
unknown <1fizzledorf@gmail.com>
2024-10-14 19:15:26 -0400

    stb_image wrapper added

---
+++ AniStudio/utils/stb_image_wrapper.cpp
@@ -0,0 +1,9 @@
+// main.cpp or any file where stb_image functions are used
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+
+#include "stb_image.h"
+#include "stb_image_write.h"
+#include "stb_image_resize.h"
+#include "stb_image_wrapper.h"
