0638621b29eb6f2019b26941724ba46bf1b66915
FizzleDorf <1fizzledorf@gmail.com>
2024-10-26 23:17:36 -0400

    sdcppsystem work ongoing

--- AniStudio/ECS/Systems/SDcppSystems/SDcppSystem.hpp
+++ AniStudio/ECS/Systems/SDcppSystems/SDcppSystem.hpp
@@ -1,139 +1,61 @@
 #pragma once
+
 #include "ECS.h"
-#include "Components.h"
+#include "Engine.hpp"
 #include "pch.h"
 #include "stable-diffusion.h"
 
 using namespace ECS;
 
+namespace ECS {
 class SDCPPSystem : public BaseSystem {
 public:
     SDCPPSystem() {
-        // Register component signatures with the system
-        AddComponentSignature<PromptComponent>();
-        AddComponentSignature<CFGComponent>();
-        AddComponentSignature<SamplerComponent>();
-        AddComponentSignature<DiffusionModelComponent>();
-        AddComponentSignature<VaeComponent>();
-        AddComponentSignature<ControlnetComponent>();
-        AddComponentSignature<EsrganComponent>();
-        AddComponentSignature<LatentComponent>();
-        AddComponentSignature<ImageComponent>();
         AddComponentSignature<InferenceComponent>();
+        AddComponentSignature<ImageComponent>();
     }
 
-    void Inference(EntityManager *mgr, EntityID entityID) {
-        // Fetch the necessary components from the entity
-        
-        const char *t5xxl_path = nullptr;
-        const char *clip_l_path = nullptr;
-        const char *clip_g_path = nullptr;
-        const char *diffusion_model_path = nullptr;
-        const char *vae_path = nullptr;
-        const char *control_net_path = nullptr;
-        const char *lora_model_dir = nullptr;
-        const char *embed_dir = nullptr;
-        const char *model_path = nullptr;
+    ~SDCPPSystem() {}
 
-        PromptComponent *prompt = nullptr;
-        if (mgr->HasComponent<PromptComponent>(entityID)) {
-            prompt = &mgr->GetComponent<PromptComponent>(entityID);
-        }
-        
-        ImageComponent *output = nullptr;
-        if (mgr->HasComponent<ImageComponent>(entityID)) {
-            output = &mgr->GetComponent<ImageComponent>(entityID);
-        }
-
-        /*CLipGComponent *clip_g = nullptr;
-        if (mgr->HasComponent<CLipGComponent>(entityID)) {
-            clip_g = &mgr->GetComponent<CLipGComponent>(entityID);
-            clip_g_path = clip_g->encoderPath.c_str();
-        }
-            
-        CLipLComponent *clip_l = nullptr;
-        if (mgr->HasComponent<CLipLComponent>(entityID)) {
-            clip_l = &mgr->GetComponent<CLipLComponent>(entityID);
-            clip_l_path = clip_l->encoderPath.c_str();
-        }
-            
-        TXXLComponent *t5xxl = nullptr;
-        if (mgr->HasComponent<TXXLComponent>(entityID)) {
-            t5xxl = &mgr->GetComponent<TXXLComponent>(entityID);
-            t5xxl_path = t5xxl->encoderPath.c_str();
-        }*/
-            
-        CFGComponent *cfg = nullptr;
-        if (mgr->HasComponent<CFGComponent>(entityID)) {
-            cfg = &mgr->GetComponent<CFGComponent>(entityID);
-        }
-
-        SamplerComponent *sampler = nullptr;
-        if (mgr->HasComponent<SamplerComponent>(entityID)) {
-            sampler = &mgr->GetComponent<SamplerComponent>(entityID);
-        }
-
-        DiffusionModelComponent *model = nullptr;
-        if (mgr->HasComponent<DiffusionModelComponent>(entityID)) {
-            model = &mgr->GetComponent<DiffusionModelComponent>(entityID);
-            model_path = model->ckptPath.c_str();
-        }
- 
-        // Prepare model paths (ensure paths are correctly set)
-         // Assuming model_path is std::string
-
-        // Context for stable diffusion model
-        sd_ctx_t *sd_context = new_sd_ctx(
-            model_path, 
-            clip_l_path, 
-            t5xxl_path, 
-            diffusion_model_path, vae_path,                         
-            "", // taesd_path                         
-            control_net_path, 
-            lora_model_dir, 
-            embed_dir,                       
-            nullptr,         // stacked_id_embed_dir_c_str                       
-            false,           // vae_decode_only                        
-            false,           // vae_tiling                        
-            false,           // free_params_immediately                        
-            4,               // n_threads                        
-            SD_TYPE_F32,     // wtype                       
-            STD_DEFAULT_RNG, // rng_type
-            DEFAULT,         // schedule
-            true,            // keep_clip_on_cpu
-            true,            // keep_control_net_cpu
-            true             // keep_vae_on_cpu
-        );
-
-        // Txt2img function
-        sd_image_t *image = txt2img(
-            sd_context, 
-            prompt->posPrompt.c_str(), 
-            nullptr, 
-            0, 
-            cfg->cfg, 
-            1.0f, 
-            512, 
-            512,
-            EULER, 
-            20, 
-            0, 
-            1, 
-            nullptr, 
-            0.0f, 
-            0.0f, 
-            true, 
-            nullptr
-        );
-
-        // Save or set the generated image in ImageComponent
-        output->SetImageData(image->data, image->width, image->height, image->channel);
-
-        // Free resources
-        free(image);
-        free_sd_ctx(sd_context);
+    void Start() override {
     }
 
-    void Update() {
-    }
+    //void Inference(EntityID entityID) {
+    //    //mgr = ANI::Core.GetManager();
+    //    inferenceComp = &mgr->GetComponent<InferenceComponent>(entityID);
+
+    //    // Prepare model paths (ensure paths are correctly set)
+    //    sd_ctx_t *sd_context = new_sd_ctx(
+    //        inferenceComp->model_path, inferenceComp->clip_l_path, inferenceComp->txxl_path,
+    //        inferenceComp->diffusion_model_path, inferenceComp->vae_path, inferenceComp->taesd_path,
+    //        inferenceComp->control_net_path, inferenceComp->lora_model_dir, inferenceComp->embed_dir,
+    //        inferenceComp->stacked_id_embed_dir_c_str, inferenceComp->vae_decode_only, inferenceComp->vae_tiling,
+    //        inferenceComp->free_params_immediately, inferenceComp->n_threads, inferenceComp->wtype,
+    //        inferenceComp->rng_type, inferenceComp->schedule, inferenceComp->keep_clip_on_cpu,
+    //        inferenceComp->keep_control_net_cpu, inferenceComp->keep_vae_on_cpu);
+
+    //    // Txt2img function
+    //    sd_image_t *image = txt2img(sd_context, "1girl", nullptr, 0, 7.0f, 1.0f, 512, 512, EULER, 20, 0, 1, nullptr,
+    //                                0.0f, 0.0f, true, nullptr);
+
+    //    // Save or set the generated image in ImageComponent
+    //    // output->SetImageData(image->data, image->width, image->height, image->channel);
+
+    //    // Free resources
+    //    free(image);
+    //    free_sd_ctx(sd_context);
+    //}
+
+    //void Update() {
+    //    if (inferenceComp->shouldInference) {
+    //        Inference(inferenceComp->GetID());
+    //        inferenceComp->shouldInference = false;
+    //    }
+    //}
+
+private:
+    ImageComponent *output = nullptr;
+    InferenceComponent *inferenceComp = nullptr;
+    EntityManager *mgr = nullptr;
 };
+} // namespace ECS
