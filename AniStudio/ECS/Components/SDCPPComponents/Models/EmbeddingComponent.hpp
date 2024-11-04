1274faef7aadf3f7d8cc6ea3b2ca23f65345f890
FizzleDorf <1fizzledorf@gmail.com>
2024-11-03 22:55:39 -0500

    EmbeddingComponent created

--- AniStudio/ECS/Components/SDCPPComponents/Models/EmbeddingComponent.hpp
+++ AniStudio/ECS/Components/SDCPPComponents/Models/EmbeddingComponent.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
 #include "BaseComponent.hpp"
+#include <string>
+
+namespace ECS {
+struct EmbeddingComponent : public ECS::BaseComponent {
+    std::string embedPath = "";
+    std::string embedName = "model.gguf";
+    bool isCkptLoaded = false;
+};
+} // namespace ECS
