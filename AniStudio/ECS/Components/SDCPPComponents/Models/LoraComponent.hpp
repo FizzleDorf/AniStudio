9474e98aa980ab854f49a4bafcc99e044e70ec2c
FizzleDorf <1fizzledorf@gmail.com>
2024-11-10 17:52:00 -0500

    fix lora comp

--- AniStudio/ECS/Components/SDCPPComponents/Models/LoraComponent.hpp
+++ AniStudio/ECS/Components/SDCPPComponents/Models/LoraComponent.hpp
@@ -5,9 +5,19 @@
 
 namespace ECS {
 struct LoraComponent : public ECS::BaseComponent {
-    std::string loraName = "";
+    std::string loraName = "model.gguf";
     std::string loraPath = "";
     float loraStrength = 1.0f; 
     float loraClipStrength = 1.0f;
+
+    LoraComponent &operator=(const LoraComponent &other) {
+        if (this != &other) { // Self-assignment check
+            loraName = other.loraName;
+            loraPath = other.loraPath;
+            loraStrength = other.loraStrength;
+            loraStrength = other.loraStrength;
+        }
+        return *this;
+    }
 };
 } 
