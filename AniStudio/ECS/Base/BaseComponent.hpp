1356d9580d435cbb258ae2c5785f62a9b5170aab
unknown <1fizzledorf@gmail.com>
2024-12-09 09:23:22 -0800

    Serialize virtual function added to base component

--- AniStudio/ECS/Base/BaseComponent.hpp
+++ AniStudio/ECS/Base/BaseComponent.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "Types.hpp"
+#include "nlohmann/json.hpp"
 
 namespace ECS {
 	struct BaseComponent {
+		std::string name = "Base_Component";
 		BaseComponent() : entityID() {}
 		virtual ~BaseComponent() {}
 		inline const EntityID GetID() const { return entityID;}
+        virtual std::string Serialize() { return NULL; }
 	private:
 		friend class EntityManager; //fren class!
 		EntityID entityID;
