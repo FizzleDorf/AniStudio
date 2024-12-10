684df340b4f040a444013836dafdf0995243bbf4
unknown <1fizzledorf@gmail.com>
2024-12-10 12:25:49 -0500

    Serialization and Deserialization defined in base component, will probably need a BaseModelComponent to reduce redundancy

--- AniStudio/ECS/Base/BaseComponent.hpp
+++ AniStudio/ECS/Base/BaseComponent.hpp
@@ -4,14 +4,31 @@
 #include "nlohmann/json.hpp"
 
 namespace ECS {
-	struct BaseComponent {
-		std::string name = "Base_Component";
-		BaseComponent() : entityID() {}
-		virtual ~BaseComponent() {}
-		inline const EntityID GetID() const { return entityID;}
-        virtual std::string Serialize() { return NULL; }
-	private:
-		friend class EntityManager; //fren class!
-		EntityID entityID;
-	};
-}
+struct BaseComponent {
+    std::string compName = "Base_Component";
+    BaseComponent() : entityID() {}
+    virtual ~BaseComponent() {}
+
+    inline const EntityID GetID() const { return entityID; }
+
+    // Serialize to JSON
+    virtual nlohmann::json Serialize() const {
+        nlohmann::json j;
+        j["compName"] = compName;
+        // j["entityID"] = entityID;
+        return j;
+    }
+
+    // Deserialize from JSON
+    virtual void Deserialize(const nlohmann::json &j) {
+        if (j.contains("compName"))
+            compName = j["compName"];
+        // if (j.contains("entityID"))
+        //     entityID = j["entityID"];
+    }
+
+private:
+    friend class EntityManager; // Friend class
+    EntityID entityID;
+};
+} // namespace ECS
--- AniStudio/ECS/Components/SDCPPComponents/ControlnetComponent.hpp
+++ AniStudio/ECS/Components/SDCPPComponents/ControlnetComponent.hpp
@@ -1,17 +1,55 @@
 #pragma once
 
 #include "BaseComponent.hpp"
-#include "stable-diffusion.h"
-#include "stb_image.h"
+#include "nlohmann/json.hpp"
 #include <string>
 
 namespace ECS {
 struct ControlnetComponent : public ECS::BaseComponent {
+    ControlnetComponent() { compName = "Controlnet_Component"; }
+
     std::string controlName = "";
     std::string controlPath = "";
-    // std::string image_reference; 
-    float cnStrength = 1.0f;            
+    float cnStrength = 1.0f;
     float applyStart = 0.0f;
-    float applyEnd = 1.0f;             
+    float applyEnd = 1.0f;
+
+    // Operator = Overload for copying a component
+     ControlnetComponent &operator=(const ControlnetComponent &other) {
+        if (this != &other) {
+            controlName = other.controlName;
+            controlPath = other.controlPath;
+            cnStrength = other.cnStrength;
+            applyStart = other.applyStart;
+            applyEnd = other.applyEnd;
+        }
+        return *this;
+    }
+
+    // Serialize to JSON
+    nlohmann::json Serialize() const override {
+        nlohmann::json j = BaseComponent::Serialize();
+        j["controlName"] = controlName;
+        j["controlPath"] = controlPath;
+        j["cnStrength"] = cnStrength;
+        j["applyStart"] = applyStart;
+        j["applyEnd"] = applyEnd;
+        return j;
+    }
+
+    // Deserialize from JSON
+    void Deserialize(const nlohmann::json &j) override {
+        BaseComponent::Deserialize(j);
+        if (j.contains("controlName"))
+            controlName = j["controlName"];
+        if (j.contains("controlPath"))
+            controlPath = j["controlPath"];
+        if (j.contains("cnStrength"))
+            cnStrength = j["cnStrength"];
+        if (j.contains("applyStart"))
+            applyStart = j["applyStart"];
+        if (j.contains("applyEnd"))
+            applyEnd = j["applyEnd"];
+    }
 };
-}
+} // namespace ECS
