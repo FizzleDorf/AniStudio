3a3c1c61ac5a46325af2b5752c5d3160819b6c68
FizzleDorf <1fizzledorf@gmail.com>
2024-12-18 18:18:40 -0500

    update imageComp

--- AniStudio/ECS/Components/ImageComponents/ImageComponent.hpp
+++ AniStudio/ECS/Components/ImageComponents/ImageComponent.hpp
@@ -1,36 +1,19 @@
 #pragma once
 
 #include "BaseComponent.hpp"
-#include "stb_image.h"
-#include "stb_image_write.h"
-#include <filesystem>
-#include <string>
 #include <GL/glew.h>
-
-#define SD_API
+#include <string>
 
 namespace ECS {
-struct ImageComponent : public ECS::BaseComponent {
+struct ImageComponent : public BaseComponent {
+    compName = "Image Component";
     std::string fileName = "image.png"; // Default file name
     std::string filePath = "";          // Full path to the image
     unsigned char *imageData = nullptr; // Pointer to image data
     int width = 0;                      // Image width
     int height = 0;                     // Image height
     int channels = 0;                   // Number of color channels
-    GLuint textureID = 0;
-
-    ImageComponent &operator=(const ImageComponent &other) {
-        if (this != &other) {
-            fileName = other.fileName;
-            filePath = other.filePath;
-            imageData = other.imageData;
-            width = other.width;
-            height = other.height;
-            channels = other.channels;
-            textureID = other.channels;
-        }
-        return *this;
-    }
+    GLuint textureID = 0;               // OpenGL texture ID
 
     ~ImageComponent() {
         if (imageData) {
@@ -38,63 +21,20 @@ struct ImageComponent : public ECS::BaseComponent {
         }
     }
 
-    bool loadImageFromPath() {
-        if (imageData) {
-            stbi_image_free(imageData);
+    ImageComponent &operator=(const ImageComponent &other) {
+        if (this != &other) { // Self-assignment check
+            fileName = other.fileName;
+            filePath = other.filePath;
+            imageData = other.imageData;
+            width = other.width;
+            height = other.height;
+            channels = other.channels;
+            textureID = other.textureID;
         }
-
-        imageData = stbi_load(filePath.c_str(), &width, &height, &channels, 0);
-        if (!imageData) {
-            return false; // Loading failed
-        }
-
-        // Update file name from the path
-        fileName = std::filesystem::path(filePath).filename().string();
-        return true;
+        return *this;
     }
-
-    void SetImageData(unsigned char *data, int imgWidth, int imgHeight, int imgChannels) {
-        if (imageData) {
-            stbi_image_free(imageData);
-        }
-
-        imageData = data;
-        width = imgWidth;
-        height = imgHeight;
-        channels = imgChannels;
-    }
-
-    bool saveImage(const std::string &outputPath) {
-        if (!imageData || width == 0 || height == 0 || channels == 0) {
-            return false; // Cannot save an invalid image
-        }
-
-        // Extract file extension
-        std::string extension = std::filesystem::path(outputPath).extension().string();
-
-        // Save based on file extension
-        bool success = false;
-        if (extension == ".png") {
-            success = stbi_write_png(outputPath.c_str(), width, height, channels, imageData, width * channels);
-        } else if (extension == ".jpg" || extension == ".jpeg") {
-            success = stbi_write_jpg(outputPath.c_str(), width, height, channels, imageData, 100); // Quality: 100
-        } else if (extension == ".bmp") {
-            success = stbi_write_bmp(outputPath.c_str(), width, height, channels, imageData);
-        } else if (extension == ".tga") {
-            success = stbi_write_tga(outputPath.c_str(), width, height, channels, imageData);
-        }
-
-        if (success) {
-            filePath = outputPath; // Update file path
-            fileName = std::filesystem::path(outputPath).filename().string();
-        }
-
-        return success;
-    }
-
-    bool saveImageAs(const std::string &outputPath) { return saveImage(outputPath); }
 };
 
-struct InputImageComponent : public ECS::ImageComponent {};
-struct OutputImageComponent : public ECS::ImageComponent {};
+struct InputImageComponent : public ImageComponent {};
+struct OutputImageComponent : public ImageComponent {};
 } // namespace ECS
