d2ff221ecc2f40c32b1294065b0917992f8cf854
FizzleDorf <1fizzledorf@gmail.com>
2025-02-13 19:54:38 -0500

    fixed diffusion serialization

--- AniStudio/Gui/base/ViewManager.hpp
+++ AniStudio/Gui/base/ViewManager.hpp
@@ -4,20 +4,33 @@
 #include "ViewTypes.hpp"
 #include "pch.h"
 
+using json = nlohmann::json;
+
 namespace GUI {
 
 class ViewManager {
 public:
+    // Constructor
     ViewManager() : viewListCount(0) {
         for (ViewListID viewList = 0u; viewList < MAX_VIEW_COUNT; viewList++) {
             availableViews.push(viewList);
         }
     }
-
+    
+    // Destructor
     ~ViewManager() = default;
 
+    // Initilization
     void Init() {}
 
+    // Render all views
+    void Render() {
+        for (const auto &viewList : viewArrays) {
+            viewList.second->RenderViews();
+        }
+    }
+
+    // Adds a viewlist
     const ViewListID CreateView() {
         const ViewListID viewList = availableViews.front();
         AddViewSignature(viewList);
@@ -26,6 +39,7 @@ public:
         return viewList;
     }
 
+    // Removes a viewlist and all of it's views
     void DestroyView(const ViewListID viewList) {
         assert(viewList < MAX_VIEW_COUNT && "ViewListID out of range!");
 
@@ -42,6 +56,7 @@ public:
         availableViews.push(viewList);
     }
 
+    // Adds a view to the viewlist by template class
     template <typename T>
     void AddView(const ViewListID viewList, T &&view) {
         assert(viewList < MAX_VIEW_COUNT && "ViewListID out of range!");
@@ -66,6 +81,7 @@ public:
         viewListCount++;
     }
 
+    // Removes a view from the viewlist by template class
     template <typename T>
     void RemoveView(const ViewListID viewList) {
         assert(viewList < MAX_VIEW_COUNT && "ViewListID out of range!");
@@ -78,12 +94,14 @@ public:
         GetViewList<T>()->Erase(viewList);
     }
 
+    // Returns a designated view type by template class
     template <typename T>
     T &GetView(const ViewListID viewList) {
         assert(viewList < MAX_VIEW_COUNT && "ViewListID out of range!");
         return GetViewList<T>()->Get(viewList);
     }
 
+    // Returns true if view is in a view list
     template <typename T>
     const bool HasView(const ViewListID viewList) {
         assert(viewList < MAX_VIEW_COUNT && "ViewListID out of range!");
@@ -98,7 +116,7 @@ public:
         return (signature.count(viewType) > 0);
     }
 
-    // View Type Registration
+    // Registers a view by string name
     template <typename T>
     void RegisterViewType(const std::string &name) {
         ViewTypeID typeId = ViewType<T>();
@@ -106,6 +124,7 @@ public:
         std::cout << "Registered view type: " << name << " with ID: " << typeId << std::endl;
     }
 
+    // Returns a designated view type by string name
     ViewTypeID GetViewType(const std::string &name) const {
         auto it = registeredViews.find(name);
         if (it != registeredViews.end()) {
@@ -115,6 +134,8 @@ public:
     }
 
     // State Management
+
+    // Resets to init state
     void Reset() {
         // Destroy all views
         for (auto &viewSignaturePair : viewSignatures) {
@@ -135,70 +156,7 @@ public:
         registeredViews.clear();
     }
 
-    // Serialization
-    void SaveState(const std::string &filepath) {
-        nlohmann::json j;
-        j["viewCount"] = viewListCount;
-
-        // Save view signatures
-        j["signatures"] = nlohmann::json::array();
-        for (const auto &[id, signature] : viewSignatures) {
-            nlohmann::json sigJson;
-            sigJson["id"] = id;
-            sigJson["types"] = nlohmann::json::array();
-            for (const auto &type : *signature) {
-                sigJson["types"].push_back(type);
-            }
-            j["signatures"].push_back(sigJson);
-        }
-
-        // Write to file
-        std::ofstream file(filepath);
-        if (file.is_open()) {
-            file << j.dump(4);
-        }
-    }
-
-    void LoadState(const std::string &filepath) {
-        std::ifstream file(filepath);
-        if (!file.is_open())
-            return;
-
-        try {
-            nlohmann::json j;
-            file >> j;
-
-            Reset();
-
-            viewListCount = j["viewCount"];
-
-            // Restore signatures
-            for (const auto &sigJson : j["signatures"]) {
-                ViewListID id = sigJson["id"].get<ViewListID>();
-                AddViewSignature(id);
-
-                // Load types into a vector first
-                std::vector<ViewTypeID> typeIds = sigJson["types"].get<std::vector<ViewTypeID>>();
-
-                // Then insert each type ID into the signature
-                for (const auto &typeId : typeIds) {
-                    GetViewSignature(id)->insert(typeId);
-                }
-            }
-
-        } catch (const std::exception &e) {
-            std::cerr << "Error loading view state: " << e.what() << std::endl;
-        }
-    }
-
-    // Render all views
-    void Render() {
-        for (const auto &viewList : viewArrays) {
-            viewList.second->RenderViews();
-        }
-    }
-
-    // Get all view IDs
+    // Return all view IDs
     std::vector<ViewListID> GetAllViews() const {
         std::vector<ViewListID> views;
         for (const auto &pair : viewSignatures) {
@@ -207,21 +165,77 @@ public:
         return views;
     }
 
+    // Returns all registered view names and types
     const std::unordered_map<std::string, ViewTypeID> &GetRegisteredViews() const { return registeredViews; }
 
+    // Returns all view list IDs and view signatures
     const std::map<ViewListID, std::shared_ptr<ViewSignature>> &GetViewSignatures() const { return viewSignatures; }
-
+    
+    // Adds a view in the view list by type
     void AddViewByType(const ViewListID viewList, const ViewTypeID viewType) {
         assert(viewList < MAX_VIEW_COUNT && "ViewListID out of range!");
         GetViewSignature(viewList)->insert(viewType);
     }
 
+    // Removes a view in the view list by type
     void RemoveViewByType(const ViewListID viewList, const ViewTypeID viewType) {
         assert(viewList < MAX_VIEW_COUNT && "ViewListID out of range!");
         GetViewSignature(viewList)->erase(viewType);
     }
 
+    // Serialization
+
+    // Serialize JSON into view lists
+    json SerializeViewLists() const {
+        json viewListsJson;
+        for (const auto &[viewListID, signature] : viewSignatures) {
+            json viewListJson;
+            viewListJson["ViewListID"] = viewListID;
+
+            json viewsJson;
+            for (const auto &viewTypeID : *signature) {
+                json viewJson;
+                viewJson["ViewTypeID"] = viewTypeID;
+
+                // Assuming you have a way to get the view instance and serialize its properties
+                // For example, if you have a method to get the view by type and ID:
+                // BaseView* view = GetViewInstance(viewListID, viewTypeID);
+                // if (view) {
+                //     viewJson["Properties"] = view->SerializeProperties();
+                // }
+
+                viewsJson.push_back(viewJson);
+            }
+            viewListJson["Views"] = viewsJson;
+
+            viewListsJson.push_back(viewListJson);
+        }
+        return viewListsJson;
+    }
+
+    // Deserialize JSON into view lists
+    void DeserializeViewLists(const json &viewListsJson) {
+        for (const auto &viewListJson : viewListsJson) {
+            ViewListID viewListID = viewListJson["ViewListID"];
+            AddViewSignature(viewListID); // Create a new ViewList
+
+            for (const auto &viewJson : viewListJson["Views"]) {
+                ViewTypeID viewTypeID = viewJson["ViewTypeID"];
+                AddViewByType(viewListID, viewTypeID); // Add the view to the ViewList
+
+                // Deserialize view properties if needed
+                // For example, if you have a method to get the view instance:
+                // BaseView* view = GetViewInstance(viewListID, viewTypeID);
+                // if (view) {
+                //     view->DeserializeProperties(viewJson["Properties"]);
+                // }
+            }
+        }
+    }
+
 private:
+
+    // Adds a new view list
     template <typename T>
     void AddViewList() {
         const ViewTypeID viewType = ViewType<T>();
@@ -229,6 +243,7 @@ private:
         viewArrays[viewType] = std::make_shared<ViewList<T>>();
     }
 
+    // Returns all view lists
     template <typename T>
     std::shared_ptr<ViewList<T>> GetViewList() {
         const ViewTypeID viewType = ViewType<T>();
@@ -238,11 +253,13 @@ private:
         return std::static_pointer_cast<ViewList<T>>(viewArrays.at(viewType));
     }
 
+    // Adds a new view signature if it doesn't exist
     void AddViewSignature(const ViewListID viewList) {
         assert(viewSignatures.find(viewList) == viewSignatures.end() && "Signature already exists");
         viewSignatures[viewList] = std::make_shared<ViewSignature>();
     }
 
+    // Returns view signatures
     std::shared_ptr<ViewSignature> GetViewSignature(const ViewListID viewList) {
         assert(viewSignatures.find(viewList) != viewSignatures.end() && "Signature Not Found");
         return viewSignatures.at(viewList);
